2e938f723abdad04b2e4acb3818f0f734f392bb2
Archit1203 <architbiz1203@gmail.com>
2021-07-30 13:09:26 -0700

    J31

--- StarPattern11.cpp
+++ StarPattern11.cpp
@@ -1,10 +1,32 @@
 /*
 n=5
 
-    * * * **
-   *      *
-  *      *
- *      *
-* * * **
+    * * * * *
+   *       *
+  *       *
+ *       *
+* * * * *
 
 */
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+                cout<<" ";
+            for(int j=1;j<=n;j++)
+                {
+                    if(j==1 || j==n || i==1 || i==n)
+                        cout<<"* ";
+                    else
+                        cout<<"  ";
+                }
+            cout<<endl;
+        }
+
+}
